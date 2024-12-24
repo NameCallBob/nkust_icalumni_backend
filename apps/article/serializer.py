1f8bfa9287a7614862b91cdcfc657008aff5d0dc
拚拚 <robin92062574@gmail.com>
2024-12-24 21:09:47 +0800

    為了照片展示及目前還是測試環境將debug打開，且文章添加多線程處理照片

--- apps/article/serializer.py
+++ apps/article/serializer.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
+from concurrent.futures import ThreadPoolExecutor
 from apps.article.models import Article, ArticleImage
 import base64
 from django.core.files.base import ContentFile
-from asgiref.sync import sync_to_async
-from django.db import transaction
 
 class ArticleImageSerializer(serializers.ModelSerializer):
     image = serializers.CharField()  # Base64 image input field
@@ -13,48 +12,36 @@ class ArticleImageSerializer(serializers.ModelSerializer):
         model = ArticleImage
         fields = ['id', 'image', 'pic_type']
 
-    async def create(self, validated_data):
+    def create(self, validated_data):
         image_data = validated_data.pop('image')
         # 解析 Base64 圖片數據
         format, imgstr = image_data.split(';base64,')
         ext = format.split('/')[-1]
         data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
-        return await ArticleImage.objects.acreate(image=data, **validated_data)
-
-    async def update(self, instance, validated_data):
-        image_data = validated_data.get('image', None)
-        if image_data:
-            format, imgstr = image_data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
-            instance.image = data
-
-        instance.pic_type = validated_data.get('pic_type', instance.pic_type)
-        await sync_to_async(instance.save)()
-        return instance
+        return ArticleImage.objects.create(image=data, **validated_data)
 
 class ArticleSerializer(serializers.ModelSerializer):
     images = ArticleImageSerializer(many=True, required=False)
 
     class Meta:
         model = Article
-        fields = [
-            'id', 'title', 'content', 'active', 'created_at', 
-            'publish_at', 'expire_at', 'view_count', 'link', 'images'
-        ]
+        fields = ['id', 'title', 'content', 'active', 'created_at', 'publish_at', 'expire_at', 'view_count', 'link', 'images']
 
-    async def create(self, validated_data):
+    def create(self, validated_data):
         images_data = validated_data.pop('images', [])
-        async with transaction.atomic():
-            article = await Article.objects.acreate(**validated_data)
+        article = Article.objects.create(**validated_data)
 
-            # 創建與文章相關的圖片
+        # 使用 ThreadPoolExecutor 處理圖片上傳
+        def process_image(image_data):
+            ArticleImageSerializer().create({**image_data, 'article': article})
+
+        with ThreadPoolExecutor(max_workers=5) as executor:  # 最多啟用 5 個執行緒
             for image_data in images_data:
-                await ArticleImageSerializer().create({**image_data, 'article': article})
+                executor.submit(process_image, image_data)
 
-            return article
+        return article
 
-    async def update(self, instance, validated_data):
+    def update(self, instance, validated_data):
         images_data = validated_data.pop('images', [])
 
         # 更新文章內容
@@ -65,28 +52,33 @@ class ArticleSerializer(serializers.ModelSerializer):
         instance.expire_at = validated_data.get('expire_at', instance.expire_at)
         instance.view_count = validated_data.get('view_count', instance.view_count)
         instance.link = validated_data.get('link', instance.link)
-        await sync_to_async(instance.save)()
+        instance.save()
 
-        # 獲取當前圖片的ID列表
-        existing_image_ids = [img.id for img in await instance.images.all()]
+        # 獲取當前圖片的 ID 列表
+        existing_image_ids = [img.id for img in instance.images.all()]
 
         # 刪除移除的圖片
-        for img in await instance.images.all():
+        for img in instance.images.all():
             if img.id not in [img_data.get('id') for img_data in images_data]:
-                await sync_to_async(img.delete)()
+                img.delete()
 
-        # 更新和新增圖片
-        for image_data in images_data:
+        # 使用 ThreadPoolExecutor 處理圖片更新
+        def process_image_update(image_data):
             image_id = image_data.get('id', None)
             if image_id and image_id in existing_image_ids:
                 # 如果圖片已經存在，則更新該圖片
-                image_instance = await ArticleImage.objects.aget(id=image_id)
-                await ArticleImageSerializer().update(image_instance, image_data)
+                image_instance = ArticleImage.objects.get(id=image_id)
+                ArticleImageSerializer().update(image_instance, image_data)
             else:
                 # 如果圖片不存在，則創建新圖片
-                await ArticleImageSerializer().create({**image_data, 'article': instance})
+                ArticleImageSerializer().create({**image_data, 'article': instance})
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            for image_data in images_data:
+                executor.submit(process_image_update, image_data)
 
         return instance
+
 class ArticleSerializer_TableOutput(serializers.ModelSerializer):
     class Meta:
         model = Article
--- apps/article/views.py
+++ apps/article/views.py
@@ -4,19 +4,15 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 from apps.article.models import Article
 from apps.article.serializer import ArticleSerializer,ArticleSerializer_TableOutput
-from asgiref.sync import sync_to_async
-from django.db import transaction
-import asyncio
 
 # drf_yasg
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from django.utils import timezone
 
 class ArticleViewSet(viewsets.ViewSet):
     """
-    處理文章的非同步 CRUD 操作，同時處理文章圖片
+    處理文章的 CRUD 操作，同時處理文章圖片
     """
 
     @swagger_auto_schema(
@@ -25,32 +21,29 @@ class ArticleViewSet(viewsets.ViewSet):
     )
     @action(methods=['get'], detail=False, authentication_classes=[], permission_classes=[permissions.AllowAny])
     def all_active(self, request):
-        async def fetch_data():
-            current_time = timezone.now()
-            queryset = await sync_to_async(list)(
-                Article.objects.filter(
-                    active=True,
-                    publish_at__lte=current_time,
-                    expire_at__gte=current_time
-                ).order_by('-created_at')
-            )
-            return ArticleSerializer(queryset, many=True).data
+        queryset = Article.objects.filter(active=True).order_by('-created_at')
+        serializer = ArticleSerializer(queryset, many=True)
+        return Response(serializer.data)
 
-        data = asyncio.run(fetch_data())
-        return Response(data)
+    @swagger_auto_schema(
+        operation_description="查詢所有文章_已發布",
+        responses={200: '成功返回所有文章'}
+    )
+    @action(methods=['get'], detail=False, authentication_classes=[], permission_classes=[permissions.AllowAny])
+    def tableOutput(self, request):
+        queryset = Article.objects.filter(active=True).order_by('-created_at')
+        serializer = ArticleSerializer_TableOutput(queryset, many=True)
+        return Response(serializer.data)
 
     @swagger_auto_schema(
         operation_description="查詢所有文章",
         responses={200: '成功返回所有文章'}
     )
     @action(methods=['get'], detail=False, authentication_classes=[], permission_classes=[permissions.AllowAny])
-    def tableOutput(self, request):
-        async def fetch_data():
-            queryset = await sync_to_async(list)(Article.objects.filter(active=True).order_by('-created_at'))
-            return ArticleSerializer_TableOutput(queryset, many=True).data
-
-        data = asyncio.run(fetch_data())
-        return Response(data)
+    def all(self, request):
+        queryset = Article.objects.all().order_by('-created_at')
+        serializer = ArticleSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     @swagger_auto_schema(
         operation_description="查詢單一文章",
@@ -65,22 +58,19 @@ class ArticleViewSet(viewsets.ViewSet):
     )
     @action(methods=['get'], detail=False, authentication_classes=[], permission_classes=[permissions.AllowAny])
     def get_one(self, request):
-        async def fetch_data():
-            try:
-                pk = request.GET['id']
-                article = await sync_to_async(Article.objects.get)(pk=pk)
-                article.view_count += 1
-                await sync_to_async(article.save)()
-                return ArticleSerializer(article).data
-            except Article.DoesNotExist:
-                return {'error': 'Not found'}, status.HTTP_404_NOT_FOUND
-            except KeyError:
-                return {'error': 'datakey not correct!'}, status.HTTP_400_BAD_REQUEST
+        try:
+            pk = request.GET['id']
+            article = Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        except KeyError:
+            return Response({'msg': "datakey not correct!"}, status=status.HTTP_400_BAD_REQUEST)
 
-        data = asyncio.run(fetch_data())
-        if isinstance(data, tuple):
-            return Response(data[0], status=data[1])
-        return Response(data)
+        article.view_count += 1
+        article.save()
+
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
 
     @swagger_auto_schema(
         operation_description="創建新文章並上傳圖片",
@@ -92,16 +82,11 @@ class ArticleViewSet(viewsets.ViewSet):
     )
     @action(methods=['post'], detail=False, authentication_classes=[JWTAuthentication], permission_classes=[permissions.IsAuthenticated])
     def new(self, request):
-        async def create_article():
-            serializer = ArticleSerializer(data=request.data)
-            if serializer.is_valid():
-                async with transaction.atomic():
-                    await sync_to_async(serializer.save)()
-                return serializer.data, status.HTTP_201_CREATED
-            return serializer.errors, status.HTTP_400_BAD_REQUEST
-
-        data, status_code = asyncio.run(create_article())
-        return Response(data, status=status_code)
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @swagger_auto_schema(
         operation_description="更新文章和圖片",
@@ -114,24 +99,19 @@ class ArticleViewSet(viewsets.ViewSet):
     )
     @action(methods=['patch'], detail=False, authentication_classes=[JWTAuthentication], permission_classes=[permissions.IsAuthenticated])
     def change(self, request):
-        async def update_article():
-            try:
-                pk = request.data.get("id", '')
-                if pk == "":
-                    return {'error': 'id not given'}, status.HTTP_400_BAD_REQUEST
-                article = await sync_to_async(Article.objects.get)(pk=pk)
-            except Article.DoesNotExist:
-                return {'error': 'Not found'}, status.HTTP_404_NOT_FOUND
+        try:
+            pk = request.data.get("id", '')
+            if pk == "":
+                return Response({"msg": "id not given"}, status=status.HTTP_400_BAD_REQUEST)
+            article = Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-            serializer = ArticleSerializer(article, data=request.data, partial=True)
-            if serializer.is_valid():
-                async with transaction.atomic():
-                    await sync_to_async(serializer.save)()
-                return serializer.data, status.HTTP_200_OK
-            return serializer.errors, status.HTTP_400_BAD_REQUEST
-
-        data, status_code = asyncio.run(update_article())
-        return Response(data, status=status_code)
+        serializer = ArticleSerializer(article, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @swagger_auto_schema(
         operation_description="刪除文章",
@@ -150,19 +130,13 @@ class ArticleViewSet(viewsets.ViewSet):
     )
     @action(methods=['delete'], detail=False, authentication_classes=[JWTAuthentication], permission_classes=[permissions.IsAuthenticated])
     def delete(self, request):
-        async def delete_article():
-            try:
-                pk = request.data.get("id", '')
-                if pk == "":
-                    return {'error': 'id not given'}, status.HTTP_400_BAD_REQUEST
-                article = await sync_to_async(Article.objects.get)(pk=pk)
-            except Article.DoesNotExist:
-                return {'error': 'Not found'}, status.HTTP_404_NOT_FOUND
+        try:
+            pk = request.data.get("id", '')
+            if pk == "":
+                return Response({"msg": "id not given"}, status=status.HTTP_400_BAD_REQUEST)
+            article = Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-            await sync_to_async(article.delete)()
-            return {}, status.HTTP_204_NO_CONTENT
-
-        data, status_code = asyncio.run(delete_article())
-        if status_code == status.HTTP_204_NO_CONTENT:
-            return Response(status=status_code)
-        return Response(data, status=status_code)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
